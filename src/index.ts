83ce4d83fa47227cd6a9dc49aa549e7305080484
Virendra patel <patelvirendra62@gmail.com>
2020-10-29 00:03:17 +0530

    ..

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { transcode } from 'buffer';
+import express, { Request, Response } from 'express'
+
+const app = express()
+
+app.listen(3000 , ()=>{
+    console.log("App is listenin......");
+})
+
+
+app.get('/' , (req : Request , res : Response)=>{
+    res.json({
+        message : 'Success.........'
+    })
+})
+
