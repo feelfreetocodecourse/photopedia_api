994a0c7a2d55fd678010101381772f2a0de1fffb
Virendra patel <patelvirendra62@gmail.com>
2020-11-01 14:27:42 +0530

    ..

--- src/controllers/picture-controller.ts
+++ src/controllers/picture-controller.ts
@@ -1,5 +1,5 @@
 import { NextFunction, Request, Response } from "express";
-
+import joi from "joi";
 export function getPictures(
   request: Request,
   response: Response,
@@ -7,10 +7,32 @@ export function getPictures(
 ) {
   response.json({ message: "All Pictures" });
 }
+
+// //  title:
+// description
+// price:
+// discount:
+// tags
 export async function createPicture(
   request: Request,
   response: Response,
   next: NextFunction
 ) {
+  const { body } = request;
+  const schema = joi.object({
+    title: joi.string().required().min(5),
+    description: joi.string(),
+    price: joi.number().required().min(1),
+    discount: joi.number(),
+    tags: joi.array().items(joi.string()),
+  });
+
+  const { value, error } = schema.validate(body);
+
+  if (error) {
+    return next(new Error(error.details[0].message));
+  }
+    console.log(value);
+    console.log(request.files);
   response.json({ message: "Create picures" });
 }
--- src/routers/picture-router.ts
+++ src/routers/picture-router.ts
@@ -1,23 +1,32 @@
 import express from "express";
-import { getPictures , createPicture} from "../controllers/picture-controller";
+import { getPictures, createPicture } from "../controllers/picture-controller";
 import { adminAuthMiddleware } from "../middlewares/auth-middlewares";
-import multer from 'multer'
-multer({dest : 'upload/' })
+import multer from "multer";
+multer({ dest: "upload/" });
 const storage = multer.diskStorage({
-    destination:  (req, file, cb)=>{
-      cb(null, 'upload/')
-    },
-    filename:  (req, file, cb)=> {
-      cb(null,  `${Date.now()}.png`)
-    }
-  })
+  destination: (req, file, cb) => {
+    cb(null, "upload/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, `${Date.now()}.png`);
+  },
+});
 
-const upload = multer({storage : storage})
+const upload = multer({ storage: storage });
+const multiUpload = upload.fields([
+  { name: "thumbnail", maxCount: 1 },
+  { name: "highQualityImage", maxCount: 1 },
+]);
 // /api/pictures
 
 const pictureRouter = express.Router();
 
-pictureRouter.get('/' , getPictures )
-pictureRouter.post('/' , adminAuthMiddleware , upload.single('thumbnail') , createPicture)
+pictureRouter.get("/", getPictures);
+pictureRouter.post(
+  "/",
+  adminAuthMiddleware,
+  multiUpload,
+  createPicture
+);
 
 export { pictureRouter };
