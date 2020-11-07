907a32e8e2e5ee8520362444b4701b5462af4c23
Virendra patel <patelvirendra62@gmail.com>
2020-11-07 18:01:01 +0530

    Update summary-controller.ts

--- src/controllers/summary-controller.ts
+++ src/controllers/summary-controller.ts
@@ -8,40 +8,57 @@ import { TokenPayload } from "../types/token-payload";
 import { OrderModel } from "../models/order";
 import { isValidObjectId } from "mongoose";
 import { nextTick } from "process";
-const { JWT_SECRET } = process.env;
+const { HOST } = process.env;
 
 async function getSaleSummary() {
-    const summary = await OrderModel.aggregate()
+  const summary = await OrderModel.aggregate()
     .match({
-        orderStatus : 'Success'
+      orderStatus: "Success",
     })
     .group({
-        _id : "$picture" , 
-        count :{
-            $sum : 1
-        }, 
-        totalSale : {
-            $sum : "$price"
-        }
+      _id: "$picture",
+      count: {
+        $sum: 1,
+      },
+      totalSale: {
+        $sum: "$price",
+      },
     })
     .lookup({
-        from: "pictures",
-       localField: "_id",
-       foreignField: "_id",
-       as: "picture"
+      from: "pictures",
+      localField: "_id",
+      foreignField: "_id",
+      as: "picture",
     })
+    .project({
+      _id: 0,
+      totalSale: 1,
+      count: 1,
+      "picture._id": 1,
+      "picture.title": 1,
+      "picture.thumbnail": 1,
+    });
 
-    return summary.map(obj=>{
-        delete obj._id
-        obj.picture = obj.picture[0]
-        return obj
-    })
+  return summary.map((e) => {
+    const picture = e.picture[0];
 
+    picture.thumbnail = `${HOST}/api/file/thumbnail/${picture._id}`;
+    e.picture = picture;
+    return e;
+  });
 }
 
 export async function summary(request: Request, response: Response) {
-    const saleSummary = await getSaleSummary()
-    response.json({
-       saleSummary
-    })
+  const saleSummary = await getSaleSummary();
+  const totalUsers = await UserModel.find().countDocuments();
+  const totalOrders = await OrderModel.find({
+    orderStatus: "Success",
+  }).countDocuments();
+  response.json({
+    summary: {
+      saleSummary,
+      totalUsers,
+      totalOrders,
+    },
+  });
 }
