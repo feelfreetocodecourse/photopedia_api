b4233666a27221d6cc0c777a61de446d2e530eb8
Virendra patel <patelvirendra62@gmail.com>
2020-11-08 07:58:03 +0530

    Update user-router.ts

--- src/routers/user-router.ts
+++ src/routers/user-router.ts
@@ -5,15 +5,16 @@ import { adminAuthMiddleware, userAuthMiddleware } from "../middlewares/auth-mid
 
 const userRouter = express.Router();
 userRouter.get("/" , adminAuthMiddleware  ,  getUsers);
+userRouter.post('/' ,  createUser )
+userRouter.put('/' , userAuthMiddleware ,   updateProfile)
 
 userRouter.get("/orders" , userAuthMiddleware , getUserOrders);
-userRouter.get("/:id" , adminAuthMiddleware  ,  getUser);
-
 userRouter.get("/orders/:orderid" , userAuthMiddleware , getUserOrder);
 userRouter.get("/isadmin" , userAuthMiddleware , isAdmin);
 userRouter.get("/me" , userAuthMiddleware , getProfile);
-userRouter.post('/' ,  createUser )
 userRouter.post('/login' ,  loginUser)
-userRouter.put('/' , userAuthMiddleware ,   updateProfile)
+
+userRouter.get("/:id" , adminAuthMiddleware  ,  getUser);
+
 
 export { userRouter };
